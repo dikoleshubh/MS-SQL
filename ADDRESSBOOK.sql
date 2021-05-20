615816fd2f98374c2a6b6dc34c301804c4f19ff7
dikoleshubh <dikole.shubh@gmail.com>
2021-05-20 20:31:44 +0530

    ADDRES BOOK DATABASE

--- ADDRESSBOOK.sql
+++ ADDRESSBOOK.sql
@@ -12,5 +12,16 @@ Zip int not null,
 PhoneNo int not null,
 Email varchar(150) not null
 );
----UC3-----
+----------UC3-Ability to insert new contacts to addressbooK-------
+USE ADDRESS_BOOK;
+INSERT INTO ADDRESS_BOOKS (FirstName,LastName,Address,City,State,Zip,PhoneNo,Email)	
+('EDUGAWA', 'CONAN','Sec-3','PUNE', 'MAHARASTRA', 452586,941144552, 'EDUGAWA@gmail.com'),
+('HERO', 'NAN','Sec-3','PUNE', 'MAHARASTRA', 452546,941144232, 'EDUGA@gmail.com'),
+('SARVES', 'S3ES','Sec-3','PUNE', 'MAHARASTRA', 452586,941144552, 'EDUGAWA@gmail.com'),
+('EDUGAWSADA', 'COASDNAN','Sec-3','PUNE', 'MAHARASTRA', 452586,941144552, 'EDUGAWA@gmail.com');
+select * from ADDRESS_BOOKS;
 
+--UC4 Ability to edit existing contact person using their name----
+use AddressBook_System;
+update Address_Book set City='MUMBAI' where FirstName='Hero' and LastName='NAN';
+select * from Address_Book;
